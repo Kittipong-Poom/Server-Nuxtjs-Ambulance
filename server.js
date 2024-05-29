005f404e35c36afd978e905958a624720491218e
KITTIPONG WONGSADITYANON <6431503005@lamduan.mfu.ac.th>
2024-05-30 00:06:44 +0700

    add .env

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ let conn = null;
 
 const Myserverambulance = async () => {
   conn = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "ambulance5",
-    port: 3306,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "ambulance5",
+    port: process.env.DB_PORT || 3306,
   });
 };
 
