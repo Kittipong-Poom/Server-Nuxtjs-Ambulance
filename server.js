37e6c99c6f7629fbd04bfa76a677390e93d18b65
KITTIPONG WONGSADITSAYANON <6431503005@lamduan.mfu.ac.th>
2024-06-12 12:50:31 -0700

    check 10.1.29.41

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let conn = null;
 const Myserverambulance = async () => {
   try { 
     conn = await mysql.createConnection({
-      host: process.env.DB_HOST ||"DESKTOP-PAL0K41",
+      host: process.env.DB_HOST ||"10.1.29.41",
       user: process.env.DB_USER || "root",
       password: process.env.DB_PASSWORD || "MFUdatabase548779",
       database: process.env.DB_NAME || "ambulance",
